f4642b907dda87b2bc19b69e2040b5545ab5cd9a
borisl <borislad1@gmail.com>
2023-06-06 22:42:39 +0300

    first commit

---
+++ stt_module/speech_to_text.py
@@ -0,0 +1,18 @@
+import speech_recognition as sr
+
+def speech_to_text():
+    recognizer = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("Speak something...")
+        audio = recognizer.listen(source)
+
+    try:
+        text = recognizer.recognize_google(audio)
+        print("You said: " + text)
+    except sr.UnknownValueError:
+        print("Sorry, I could not understand your speech.")
+    except sr.RequestError as e:
+        print("Speech recognition service is unavailable. Error: " + str(e))
+
+speech_to_text()

3dadbe04e891ba6a0686834a34ca167720ec1156
borisl <borislad1@gmail.com>
2023-07-08 12:23:22 +0300

    stt_module

--- Learning_Recommendation/visualization/histogram.py
+++ Learning_Recommendation/visualization/histogram.py
@@ -9,9 +9,13 @@ def plot_sorted_dict(my_dict, title):
     plt.xlabel(title)
     plt.ylabel('Num. of tries')
     plt.title(title + " plot")
+    plt.savefig(f"{title}_plot/" + title + "_histogram.png")
     plt.show()
 
 
 # Example usage
-# my_dict = {'Conscience': 4, 'Resilience': 3, 'Competence': 2, 'Providence': 3, 'Elegance': 1}
-# plot_sorted_dict(my_dict, "Syllable/Word")
+my_dict = {'Conscience': 4, 'Resilience': 3, 'Competence': 2, 'Providence': 3, 'Elegance': 1}
+plot_sorted_dict(my_dict, "word")
+
+# After creating the visualization function, we need to save it as a png file.
+# We will use the savefig() function to save the plot as a png file.
--- learning_process_moudle/learning_process.py
+++ learning_process_moudle/learning_process.py
@@ -1,19 +1,23 @@
 import csv
 import os
+
+from Learning_Recommendation.visualization.histogram import plot_sorted_dict
 from word_verification_module.file_utils import build_file_path
 from Learning_Recommendation.Learning_Recommendation import find_highest_value
 from word_verification_module.syllable_verification import Syllable_verifaction
 from word_generator.random_word import find_words_by_syllable
 from word_verification_module.word_verification import word_verification_test
 
-def learning_steps():
 
+def learning_steps():
     syllable_dict = Syllable_verifaction()
     Syllable_To_Practice = find_highest_value(syllable_dict)
     # NOW NEED TO ADD WITCH FAMILY OF WORDS TO TAKE BASED ON THE Syllable_To_Practice key!!
     Words_to_practice = find_words_by_syllable(Syllable_To_Practice)
     print('The words to practice are:', ', '.join(Words_to_practice))
     word_verification_test(Words_to_practice)
-    #ADD plots to show the learning rate...
+    # ADD plots to show the learning rate...
+    plot_sorted_dict(Syllable_To_Practice, "syllable")
+    plot_sorted_dict(Words_to_practice, "word")
     print('ADD plots to show the learning rate...')
 # learning_steps()
